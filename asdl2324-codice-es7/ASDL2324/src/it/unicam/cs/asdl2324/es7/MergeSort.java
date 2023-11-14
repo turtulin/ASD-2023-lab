ce81cdd907e55406c953eee11a8ba784a97734a9
turtulin <mm.martamusso@gmail.com>
2023-11-14 17:55:50 +0100

    [add] Added method mergeSort to divide the List. Started to write method merge to put togheter all Lists

--- asdl2324-codice-es7/ASDL2324/src/it/unicam/cs/asdl2324/es7/MergeSort.java
+++ asdl2324-codice-es7/ASDL2324/src/it/unicam/cs/asdl2324/es7/MergeSort.java
@@ -21,6 +21,33 @@ public class MergeSort<E extends Comparable<E>> implements SortingAlgorithm<E> {
         return null;
     }
 
+    private void mergeSort(List<E> l, int left, int right) {
+        if(left > right || (left <= 1 && right <= 1)) {
+            throw new IllegalArgumentException("La parte sinistra deve essere più piccola di quella a destra");
+        }
+        int middle = (left + right) / 2;
+        mergeSort(l, left, middle);
+        mergeSort(l, middle + 1, right);
+        merge(l, left, middle, right);
+    }
+
+    private void merge(List<E> l, int left, int middle, int right) {
+        int m1 = middle - left + 1;
+        int m2 = right - middle;
+        List<E> B = l.subList(left, middle);
+        List<E> C = l.subList(middle + 1, left);
+
+        int i = 1;
+        int j = 1;
+        int k = left;
+
+        while(i < m1 && j < m2) {
+            if(B.get(i).compareTo(C.get(j)) <= 0) {
+                l.set();
+            }
+        }
+    }
+
     public String getName() {
         return "MergeSort";
     }
