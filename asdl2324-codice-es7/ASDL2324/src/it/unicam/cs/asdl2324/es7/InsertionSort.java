6060eb31c4ea592d384464f8bef36b90db3c34fb
turtulin <mm.martamusso@gmail.com>
2023-11-14 16:43:44 +0100

    [add] Added method for Insertion Sort

--- asdl2324-codice-es7/ASDL2324/src/it/unicam/cs/asdl2324/es7/InsertionSort.java
+++ asdl2324-codice-es7/ASDL2324/src/it/unicam/cs/asdl2324/es7/InsertionSort.java
@@ -17,9 +17,24 @@ public class InsertionSort<E extends Comparable<E>>
         implements SortingAlgorithm<E> {
 
     public SortingAlgorithmResult<E> sort(List<E> l) {
-        // TODO implementare
+        if(l == null) throw new NullPointerException("Tentativo di ordinare una lista null");
+        if(l.size() <= 1) return new SortingAlgorithmResult<>(l, 0);
+
+        int count = 0;
+
+        for(int j = 1; j < l.size(); j++) {
+            E temp = l.get(j);
+            int i = j - 1;
+            while (i >= 0 && l.get(i).compareTo(temp) > 0) {
+                l.set(i + 1, l.get(i));
+                i--;
+            }
+            l.set(i + 1, temp);
+            count += j - i;
+        }
         return null;
     }
+
 
     public String getName() {
         return "InsertionSort";
