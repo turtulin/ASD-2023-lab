2699e0a8a1c7ad23387df5372d0fbd1fd69d7487
turtulin <mm.martamusso@gmail.com>
2023-11-21 14:00:55 +0100

    [edit] Edited class LinkedListDisjointSets and completed correctly. Every test is correct
    [TODO] Write better comments, put the code in a better way, little changes in choices like null exception

--- asdl2324-codice-mp1/ASDL2324/src/it/unicam/cs/asdl2324/mp1/LinkedListDisjointSets.java
+++ asdl2324-codice-mp1/ASDL2324/src/it/unicam/cs/asdl2324/mp1/LinkedListDisjointSets.java
@@ -1,25 +1,34 @@
 package it.unicam.cs.asdl2324.mp1;
 
+import java.util.Objects;
 import java.util.Set;
-
-// TODO insireire import della Java SE che si ritengono necessari
+import java.util.HashSet;
+import java.util.HashMap;
 
 /**
- * // TODO spiegare come viene implementata la classe
- * 
- * @author Luca Tesei (template) **INSERIRE NOME, COGNOME ED EMAIL
- *         xxxx@studenti.unicam.it DELLO STUDENTE** (implementazione)
+ * La classe che implementa l'interfaccia {@code DisjointSets} usando le liste concatenate
+ * per rappresentare gli insiemi disgiunti.
+ * Per memorizzare e manipolare gli insiemi disgiunti, usa una mappa che associa ad ogni
+ * elemento il suo rappresentante e un set che contiene tutti i rappresentanti
+ * degli insiemi disgiunti attualmente presenti.
+ *
+ * @author  Luca Tesei (template)
+ *          Marta Musso marta.musso@studenti.unicam.it (implementazione)
  *
  */
 public class LinkedListDisjointSets implements DisjointSets {
-
-    // TODO inserire le variabili istanza private che si ritengono necessarie
+    // La mappa che associa ad ogni elemento il suo rappresentante
+    private HashMap<DisjointSetElement, DisjointSetElement> map;
+    // Il set dei rappresentanti degli insiemi disgiunti attualmente presenti
+    private HashSet<DisjointSetElement> representatives;
 
     /**
      * Crea una collezione vuota di insiemi disgiunti.
      */
     public LinkedListDisjointSets() {
-        // TODO implementare
+        // Inizializza la mappa e il set come vuoti
+        this.map = new HashMap<DisjointSetElement, DisjointSetElement>();
+        this.representatives = new HashSet<DisjointSetElement>();
     }
 
     /*
@@ -29,19 +38,41 @@ public class LinkedListDisjointSets implements DisjointSets {
      */
     @Override
     public boolean isPresent(DisjointSetElement e) {
-        // TODO implementare
-        return false;
+        // Controllo che e non sia null
+        // Come nella classe MyMultiset, anzichè usare
+        // if (e == null) throw new NullPointerException("Messaggio");
+        // utilizzo un metodo statico della classe Objects che richiede
+        // solo una chiamata e un messaggio opzionale, rendendo il codice
+        // più compatto, leggibile ed efficiente
+        Objects.requireNonNull(e, "Tentativo di verificare la presenza di un elemento null");
+        // Controllo se la mappa contiene e come chiave
+        return this.map.containsKey(e);
     }
 
     /*
      * Nella rappresentazione con liste concatenate un nuovo insieme disgiunto è
      * rappresentato da una lista concatenata che contiene l'unico elemento. Il
-     * rappresentante deve essere l'elemento stesso e la cardinalità deve essere
-     * 1.
+     * rappresentante deve essere l'elemento stesso e la cardinalità deve essere 1.
      */
     @Override
     public void makeSet(DisjointSetElement e) {
-        // TODO implementare
+        // Controllo  che e non sia null
+        Objects.requireNonNull(e, "Tentativo di creare un insieme disgiunto con un elemento null");
+        // Controllo che non sia già presente nella collezione
+        if (this.isPresent(e)) {
+            throw new IllegalArgumentException(
+                    "Tentativo di creare un insieme disgiunto con un elemento già presente");
+        }
+        // Imposto il puntatore ref1 di e a se stesso
+        e.setRef1(e);
+        // Imposto il puntatore ref2 di e a null
+        e.setRef2(null);
+        // Imposto il numero di e a 1
+        e.setNumber(1);
+        // Inserisco la coppia (e, e) nella mappa
+        this.map.put(e, e);
+        // Inserisco e nel set dei rappresentanti
+        this.representatives.add(e);
     }
 
     /*
@@ -51,8 +82,15 @@ public class LinkedListDisjointSets implements DisjointSets {
      */
     @Override
     public DisjointSetElement findSet(DisjointSetElement e) {
-        // TODO implementare
-        return null;
+        // Controllo che è non sia null
+        Objects.requireNonNull(e, "Tentativo di trovare il rappresentante di un elemento null");
+        // Controllo che e sia presente nella collezione
+        if (!this.isPresent(e)) {
+            throw new IllegalArgumentException(
+                    "Tentativo di trovare il rappresentante di un elemento non presente");
+        }
+        // Restituisce il valore associato a e nella mappa
+        return this.map.get(e);
     }
 
     /*
@@ -71,26 +109,126 @@ public class LinkedListDisjointSets implements DisjointSets {
      */
     @Override
     public void union(DisjointSetElement e1, DisjointSetElement e2) {
-        // TODO implementare
+        // Controllo che e1 ed e2 non siano null
+        Objects.requireNonNull(e1, "Tentativo di unire due insiemi disgiunti" +
+                "con il primo elemento null");
+        Objects.requireNonNull(e2, "Tentativo di unire due insiemi disgiunti" +
+                "con il secondo elemento null");
+        // Controlla che e1 ed e2 siano presenti nella collezione
+        if (!this.isPresent(e1) || !this.isPresent(e2)) {
+            throw new IllegalArgumentException(("Tentativo di unire due insiemi disgiunti" +
+                    "con almeno un elemento non presente"));
+        }
+        // Trovo i rappresentanti di e1 ed e2 usando la mappa
+        DisjointSetElement r1 = this.map.get(e1);
+        DisjointSetElement r2 = this.map.get(e2);
+        // Se r1 e r2 sono uguali, termino l'operazione
+        if (r1 == r2) {
+            return;
+        }
+        // Altrimenti, unisco i due insiemi disgiunti
+        // Trovo le cardinalità dei due insiemi disgiunti usando il numero di r1 e r2
+        int c1 = r1.getNumber();
+        int c2 = r2.getNumber();
+        // Se c1 è maggiore o uguale a c2, appendo la lista di r2 alla fine di quella di 21
+        if (c1 >= c2) {
+            // Trovo l'ultimo elemento della lista di r1
+            DisjointSetElement last1 = r1;
+            while (last1.getRef2() != null) {
+                last1 = last1.getRef2();
+            }
+            // Imposto il puntatore ref2 di last1 a r2
+            last1.setRef2(r2);
+            // Imposto il puntatore ref1 di r2 a r1
+            r2.setRef1(r1);
+            // Aggiorno il numero di r1
+            r1.setNumber(c1 + c2);
+            // Scorro la lista di r2 e aggiorno i puntatori ref1 e la mappa
+            DisjointSetElement current = r2;
+            while (current != null) {
+                // Imposto il puntatore ref1 di current a r1
+                current.setRef1(r1);
+                // Aggiorno la mappa
+                this.map.put(current, r1);
+                // Passo al prossimo elemento
+                current = current.getRef2();
+            }
+            // Rimuovo r2 dal set dei rappresentanti
+            this.representatives.remove(r2);
+        }
+        // Se c1 è minore di c2, appendo la lista di r1 alla fine di quella di r2
+        else {
+            // Trovo l'ultimo elemento delle lista di r2
+            DisjointSetElement last2 = r2;
+            while (last2.getRef2() != null) {
+                last2 = last2.getRef2();
+            }
+            // Imposto il puntatore ref2 di last2 a r1
+            last2.setRef2(r1);
+            // Imposto il puntatore ref1 di r1 a r2
+            r1.setRef1(r2);
+            // Aggiorno il numero di r2
+            r2.setNumber(c1 + c2);
+            // Scorro la lista di r1 e aggiorno i puntatori ref1 e la mappa
+            DisjointSetElement current = r1;
+            while (current != null) {
+                // Imposto il puntatore ref1 di current a r2
+                current.setRef1(r2);
+                // Aggiorno la mappa
+                this.map.put(current, r2);
+                // Passo al prossimo elemento
+                current = current.getRef2();
+            }
+            // Rimuovi r1 dal set dei rappresentanti
+            this.representatives.remove(r1);
+        }
     }
 
     @Override
     public Set<DisjointSetElement> getCurrentRepresentatives() {
-        // TODO implementare
-        return null;
+        return this.representatives;
     }
 
     @Override
-    public Set<DisjointSetElement> getCurrentElementsOfSetContaining(
-            DisjointSetElement e) {
-        // TODO implementare
-        return null;
+    public Set<DisjointSetElement> getCurrentElementsOfSetContaining(DisjointSetElement e) {
+        // Controllo che e non sia null
+        Objects.requireNonNull(e, "Tentativo di ottenere gli elementi di un" +
+                "insieme disgiunto contenente un elemento null");
+        // Controllo che sia presente nella collezione
+        if (!this.isPresent(e)) {
+            throw new IllegalArgumentException("Tentativo di ottenere gli elementi di un" +
+                    "insieme disgiunto contenente un elemento non presente");
+        }
+        // Creo un nuovo set vuoto di tipo DisjointSetElement
+        Set<DisjointSetElement> result = new HashSet<DisjointSetElement>();
+        // Trovo il rappresentante dell'insieme disgiunto di e usando la mappa
+        DisjointSetElement r = this.map.get(e);
+        // Scorro la lista concatenata di r e aggiungi ogni elemento al set
+        DisjointSetElement current = r;
+        while (current != null) {
+            // Aggiungo current al set
+            result.add(current);
+            // Passo al prossimo elemento
+            current = current.getRef2();
+        }
+        // Restituisci il set creato
+        return result;
     }
 
     @Override
     public int getCardinalityOfSetContaining(DisjointSetElement e) {
-        // TODO implementare
-        return -1;
+        // Controllo che e non sia null
+        Objects.requireNonNull(e, "Tentativo di ottenere la cardinalità di un insieme" +
+                "disgiunto contenente un elemento null");
+        // Controllo che e sia presente nella collezione
+        if(!this.isPresent(e)) {
+            throw new IllegalArgumentException("Tentativo di ottenere la cardinalità di un insieme" +
+                    "dsgiunto contenente un elemento non presente");
+        }
+        // Trovo il rappresentante dell'insieme disgiunto di e usando la mappa
+        DisjointSetElement r = this.map.get(e);
+        // Restituisce il numero di r, che corrisponde alla cardinalità dell'insieme
+        // disgiunto di e
+        return r.getNumber();
     }
-
 }
