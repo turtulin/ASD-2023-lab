79b1468b01b6f7787a01f0053edd635614a38857
Marta <129783246+turtulin@users.noreply.github.com>
2024-05-23 11:31:10 +0200

    Update CollisionListResizableHashTable.java

--- asdl2223-codice-es10/ASDL2223/src/it/unicam/cs/asdl2223/es10/CollisionListResizableHashTable.java
+++ asdl2223-codice-es10/ASDL2223/src/it/unicam/cs/asdl2223/es10/CollisionListResizableHashTable.java
@@ -1,109 +1,23 @@
-/**
- * 
- */
 package it.unicam.cs.asdl2223.es10;
 
 import java.util.*;
 
-/**
- * Realizza un insieme tramite una tabella hash con indirizzamento primario (la
- * funzione di hash primario deve essere passata come parametro nel costruttore
- * e deve implementare l'interface PrimaryHashFunction) e liste di collisione.
- * 
- * La tabella, poiché implementa l'interfaccia Set<E> non accetta elementi
- * duplicati (individuati tramite il metodo equals() che si assume sia
- * opportunamente ridefinito nella classe E) e non accetta elementi null.
- * 
- * La tabella ha una dimensione iniziale di default (16) e un fattore di
- * caricamento di defaut (0.75). Quando il fattore di bilanciamento effettivo
- * eccede quello di default la tabella viene raddoppiata e viene fatto un
- * riposizionamento di tutti gli elementi.
- * 
- * @author Template: Luca Tesei, Implementazione: collettiva
- *
- */
 public class CollisionListResizableHashTable<E> implements Set<E> {
-
-    /*
-     * La capacità iniziale. E' una potenza di due e quindi la capacità sarà
-     * sempre una potenza di due, in quanto ogni resize raddoppia la tabella.
-     */
     private static final int INITIAL_CAPACITY = 16;
-
-    /*
-     * Fattore di bilanciamento di default. Tipico valore.
-     */
     private static final double LOAD_FACTOR = 0.75;
-
-    /*
-     * Numero di elementi effettivamente presenti nella hash table in questo
-     * momento. ATTENZIONE: questo valore è diverso dalla capacity, che è la
-     * lunghezza attuale dell'array di Object che rappresenta la tabella.
-     */
     private int size;
-
-    /*
-     * L'idea è che l'elemento in posizione i della tabella hash è un bucket che
-     * contiene null oppure il puntatore al primo nodo di una lista concatenata
-     * di elementi. Si può riprendere e adattare il proprio codice della
-     * Esercitazione 6 che realizzava una lista concatenata di elementi
-     * generici. La classe interna Node<E> è ripresa proprio da lì.
-     * 
-     * ATTENZIONE: la tabella hash vera e propria può essere solo un generico
-     * array di Object e non di Node<E> per una impossibilità del compilatore di
-     * accettare di creare array a runtime con un tipo generics. Ciò infatti
-     * comporterebbe dei problemi nel sistema di check dei tipi Java che, a
-     * run-time, potrebbe eseguire degli assegnamenti in violazione del tipo
-     * effettivo della variabile. Quindi usiamo un array di Object che
-     * riempiremo sempre con null o con puntatori a oggetti di tipo Node<E>.
-     * 
-     * Per inserire un elemento nella tabella possiamo usare il polimorfismo di
-     * Object:
-     * 
-     * this.table[i] = new Node<E>(item, next);
-     * 
-     * ma quando dobbiamo prendere un elemento dalla tabella saremo costretti a
-     * fare un cast esplicito:
-     * 
-     * Node<E> myNode = (Node<E>) this.table[i];
-     * 
-     * Ci sarà dato un warning di cast non controllato, ma possiamo eliminarlo
-     * con un tag @SuppressWarning,
-     */
     private Object[] table;
-
-    /*
-     * Funzion di hash primaria usata da questa hash table. Va inizializzata nel
-     * costruttore all'atto di creazione dell'oggetto.
-     */
     private final PrimaryHashFunction phf;
-
-    /*
-     * Contatore del numero di modifiche. Serve per rendere l'iterator
-     * fail-fast.
-     */
     private int modCount;
 
-    // I due metodi seguenti sono di comodo per gestire la capacity e la soglia
-    // oltre la quale bisogna fare il resize.
-
-    /* Numero di elementi della tabella corrente */
     private int getCurrentCapacity() {
         return this.table.length;
     };
 
-    /*
-     * Valore corrente soglia oltre la quale si deve fare la resize,
-     * getCurrentCapacity * LOAD_FACTOR
-     */
     private int getCurrentThreshold() {
         return (int) (getCurrentCapacity() * LOAD_FACTOR);
     }
 
-    /**
-     * Costruisce una Hash Table con capacità iniziale di default e fattore di
-     * caricamento di default.
-     */
     public CollisionListResizableHashTable(PrimaryHashFunction phf) {
         this.phf = phf;
         this.table = new Object[INITIAL_CAPACITY];
@@ -180,10 +94,6 @@ public class CollisionListResizableHashTable<E> implements Set<E> {
         return true;
     }
 
-    /*
-     * Raddoppia la tabella corrente e riposiziona tutti gli elementi. Da
-     * chiamare quando this.size diventa maggiore di getCurrentThreshold()
-     */
     private void resize() {
             Object[] oldTable = table;
             table = new Object[oldTable.length * 2];
@@ -283,39 +193,22 @@ public class CollisionListResizableHashTable<E> implements Set<E> {
 
     @Override
     public void clear() {
-        // Ritorno alla situazione iniziale
         this.table = new Object[INITIAL_CAPACITY];
         this.size = 0;
         this.modCount = 0;
     }
 
-    /*
-     * Classe per i nodi della lista concatenata. Lo specificatore è protected
-     * solo per permettere i test JUnit.
-     */
     protected static class Node<E> {
         protected E item;
 
         protected Node<E> next;
 
-        /*
-         * Crea un nodo "singolo" equivalente a una lista con un solo elemento.
-         */
         Node(E item, Node<E> next) {
             this.item = item;
             this.next = next;
         }
     }
 
-    /*
-     * Classe che realizza un iteratore per questa hash table. L'ordine in cui
-     * vengono restituiti gli oggetti presenti non è rilevante, ma ogni oggetto
-     * presente deve essere restituito dall'iteratore una e una sola volta.
-     * L'iteratore deve essere fail-fast, cioè deve lanciare una eccezione
-     * ConcurrentModificationException se a una chiamata di next() si "accorge"
-     * che la tabella è stata cambiata rispetto a quando l'iteratore è stato
-     * creato.
-     */
     private class Itr implements Iterator<E> {
         private int numeroModificheAtteso;
         private int currentIndex;
@@ -365,16 +258,10 @@ public class CollisionListResizableHashTable<E> implements Set<E> {
         }
     }
 
-    /*
-     * Only for JUnit testing purposes.
-     */
     protected Object[] getTable() {
         return this.table;
     }
 
-    /*
-     * Only for JUnit testing purposes.
-     */
     protected PrimaryHashFunction getPhf() {
         return this.phf;
     }
