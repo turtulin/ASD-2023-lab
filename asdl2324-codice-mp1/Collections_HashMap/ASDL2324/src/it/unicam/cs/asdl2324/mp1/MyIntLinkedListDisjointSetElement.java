a04f2f8fe9d49592de7bdd6cca70c96fa78032b3
Marta <129783246+turtulin@users.noreply.github.com>
2024-05-23 12:01:08 +0200

    Update MyIntLinkedListDisjointSetElement.java

--- asdl2324-codice-mp1/Collections_HashMap/ASDL2324/src/it/unicam/cs/asdl2324/mp1/MyIntLinkedListDisjointSetElement.java
+++ asdl2324-codice-mp1/Collections_HashMap/ASDL2324/src/it/unicam/cs/asdl2324/mp1/MyIntLinkedListDisjointSetElement.java
@@ -1,62 +1,22 @@
 package it.unicam.cs.asdl2324.mp1;
 
-/**
- * Una semplice classe che implementa l'interface {@code DisjointSetElement} i
- * cui elementi sono interi e che è adatta per essere usata da una
- * implementazione dell'interface {@code DisjointSets} che usa liste
- * concatenate.
- * 
- * @author Luca Tesei
- *
- */
 public class MyIntLinkedListDisjointSetElement implements DisjointSetElement {
 
-    /*
-     * L'intero che rappresenta questo elemento
-     */
     private final int value;
 
-    /*
-     * Puntatore all'elemento rappresentante, cioè il primo della lista
-     * concatenata che rappresenta l'insieme disgiunto di questo elemento
-     */
     private DisjointSetElement representative;
 
-    /*
-     * Puntatore al prossimo elemento nella lista concatenata che rappresenta
-     * l'insieme disgiunto di cui questo elemento fa parte
-     */
     private DisjointSetElement nextElement;
 
-    /*
-     * Numero di elementi di questo insieme disgiunto, diverso da zero solo se
-     * questo elemento è il rappresentante, altrimenti non viene usato
-     */
     private int size;
 
-    /**
-     * Crea un nuovo elemento di un insieme disgiunto.
-     * 
-     * @param value
-     *                  il numero associato al nuovo elemento
-     */
     public MyIntLinkedListDisjointSetElement(int value) {
         this.value = value;
-        /*
-         * Metto i campi relativi al DisjointSet ai valori di default. Nelle API
-         * dell'interface LinkedListDisjointSets è indicato che tali valori sono
-         * quelli appropriati da mettere nel costruttore della classe che
-         * implementa l'interface. Indicano che l'elemento appena creato non è
-         * stato ancora inserito in nessun disjoint set.
-         */
         this.representative = null;
         this.nextElement = null;
         this.size = 0;
     }
 
-    /**
-     * @return the value
-     */
     public int getValue() {
         return value;
     }
@@ -91,11 +51,6 @@ public class MyIntLinkedListDisjointSetElement implements DisjointSetElement {
         this.size = n;
     }
 
-    /*
-     * Si usa solo il value per l'hashcode poiché l'uso di
-     * representative.hashcode() potrebbe produrre delle chiamate ricorsive che
-     * non terminano.
-     */
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -104,10 +59,6 @@ public class MyIntLinkedListDisjointSetElement implements DisjointSetElement {
         return result;
     }
 
-    /*
-     * L'uguaglianza è definita in base al rappresentante e al valore, utile per
-     * scrivere i test JUnit in maniera più semplice.
-     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
